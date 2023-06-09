358805dcae02adba69231b470e57599d292fbf21
Abhradip Saha <abhradip360saha@gmail.com>
2023-06-10 00:33:40 +0530

    todo

---
+++ index.js
@@ -0,0 +1,42 @@
+const box=document.getElementById("box");
+const  list=document.getElementById("list");
+function addTask()
+{
+    if(box.value==='')
+       alert("Write Something!");
+    else
+      {
+        let li=document.createElement("li");
+        li.innerHTML=box.value;
+        list.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+      }
+      box.value = "";
+      saveData();
+}
+
+list.addEventListener("click",function(e){
+    // if(e.target.tagname==="LI")
+    // {
+    //    e.target.classList.toggle("checked");
+    //    saveData();
+    // }
+    // else 
+    if(e.target.tagName==="SPAN")
+    {
+       e.target.parentElement.remove();
+       saveData();
+    }
+        
+},false);
+function saveData()
+{
+    localStorage.setItem("data",list.innerHTML);
+}
+function showTask()
+{
+    list.innerHTML=localStorage.getItem("data");
+}
+showTask();
